24e52c38c778f529ba570def8c7148a2c7b105ef
harsh306 <hpathak@expedia.com>
2021-02-04 21:13:13 -0800

    tests dir, setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="continuation-jax", # Replace with your own username
+    version="0.0.1",
+    author="Harsh Nilesh Pathak",
+    author_email="harshnpathak@gmail.com",
+    description="Continuation Methods for Deep Neural Networks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harsh306/continuation-jax",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires='>=3.6',
+)
---
+++ tests/utils/test_pytree_dot.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestPytree_dot(TestCase):
+    pass
