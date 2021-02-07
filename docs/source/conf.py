96781562bdaf31ec9ba9da39112a1ee02cc62664
harsh306 <hpathak@expedia.com>
2021-02-06 18:00:47 -0800

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_theme = "default"
+#html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 RTD_NEW_THEME = True
 autosummary_generate = True
 html_static_path = ["_static"]
