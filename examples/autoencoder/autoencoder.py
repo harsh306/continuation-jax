d62f21aaae5637c08b57967d83564d108838db37
harsh306 <hpathak@expedia.com>
2021-02-19 17:02:04 -0800

    bigger AE and constant init for jax

--- examples/autoencoder/autoencoder.py
+++ examples/autoencoder/autoencoder.py
@@ -1,7 +1,7 @@
 import jax.numpy as np
 import numpy as onp
 from jax.experimental import stax
-from jax.nn.initializers import zeros
+from jax.nn.initializers import zeros, ones
 from jax.nn import sigmoid
 from jax.experimental.optimizers import l2_norm
 from jax.experimental.stax import Dense, elementwise, Identity
@@ -11,7 +11,7 @@ from examples.abstract_problem import AbstractProblem
 from jax.tree_util import tree_map
 
 batch_size = 8
-input_shape = (8, 2)
+input_shape = (8, 8)
 step_size = 0.1
 num_steps = 10
 code_dim = 1
@@ -21,10 +21,25 @@ def homotopy_activation(x, alpha=1.0, activation_func=None):
     return alpha * x + (1 - alpha) * activation_func(x)
 
 
-def pca_orthogonal(v, dtype=np.float32):
-    def init(key, shape, dtype=dtype):
-        return v[: shape[0], : shape[1]]
+def constant_init(m, dtype=np.float32):
+    """
+    Constant weight initializer
+    Args:
+        m: constant matrix
+        dtype: matrix dtype
 
+    Returns:
+        init function
+    """
+    def init(key, shape, dtype=dtype):
+        return m[:shape[0], :shape[1]]
+    return init
+
+def constant(matrix, dtype=np.float32):
+    def init(key, shape, dtype=dtype):
+        if matrix.shape != shape:
+            raise ValueError(f"input array of shape {matrix.shape} is not equal to the weight array of shape {shape}")
+        return matrix
     return init
 
 
@@ -37,7 +52,9 @@ def synth_batches():
 batches = synth_batches()
 inputs = next(batches)
 u, s, v_t = onp.linalg.svd(inputs, full_matrices=False)
-
+I = np.eye(v_t.shape[-1])
+I_add =npr.normal(0.0, 0.008, size=I.shape)
+noisy_I = I + I_add
 
 def HomotopyDense(out_dim, W_init=zeros, b_init=zeros):
     """Layer constructor function for a dense (fully-connected) layer."""
@@ -49,19 +66,19 @@ def HomotopyDense(out_dim, W_init=zeros, b_init=zeros):
         return output_shape, (W, b)
 
     def apply_fun(params, inputs, **kwargs):
-        bparam = kwargs.get("bparam")
         W, b = params
         dense = np.dot(inputs, W) + b
         return homotopy_activation(
-            dense, alpha=bparam, activation_func=kwargs.get("activation_func")
+            dense, alpha=kwargs.get("bparam"), activation_func=kwargs.get("activation_func")
         )
-
     return init_fun, apply_fun
 
 
 init_fun, predict_fun = stax.serial(
-    HomotopyDense(out_dim=code_dim, W_init=pca_orthogonal(v_t.T), b_init=zeros),
-    HomotopyDense(out_dim=input_shape[-1], W_init=pca_orthogonal(v_t), b_init=zeros),
+    HomotopyDense(out_dim=4, W_init=constant_init(v_t.T), b_init=zeros),
+    HomotopyDense(out_dim=2, W_init=constant_init(noisy_I), b_init=zeros),
+    HomotopyDense(out_dim=4, W_init=constant_init(noisy_I), b_init=zeros),
+    Dense(out_dim=input_shape[-1], W_init=constant_init(v_t), b_init=zeros),
 )
 
 
@@ -73,18 +90,18 @@ class PCATopologyAE(AbstractProblem):
     def objective(params, bparam) -> float:
         logits = predict_fun(params, inputs, bparam=bparam[0], activation_func=sigmoid)
         loss = np.mean((np.subtract(logits, inputs)))
-        loss += l2_norm(params) + l2_norm(bparam)
+        #loss += l2_norm(params) + l2_norm(bparam)
         return loss
 
     def initial_value(self):
         ae_shape, ae_params = init_fun(random.PRNGKey(0), input_shape)
         assert ae_shape == input_shape
-        bparam = [np.array([0.10], dtype=np.float64)]
+        bparam = [np.array([0.00], dtype=np.float64)]
         return ae_params, bparam
 
     def initial_values(self):
         state, bparam = self.initial_value()
-        state_1 = tree_map(lambda a: a + 0.05, state)
+        state_1 = tree_map(lambda a: a + 0.005, state)
         states = [state, state_1]
         bparam_1 = tree_map(lambda a: a + 0.05, bparam)
         bparams = [bparam, bparam_1]
@@ -96,3 +113,8 @@ if __name__ == "__main__":
     ae_params, bparams = problem.initial_value()
     loss = problem.objective(ae_params, bparams)
     print(loss)
+
+    init_c = constant(matrix=I)
+    print(init_c(key=0, shape=(8,8)))
+
+
--- utils/custom_nn.py
+++ utils/custom_nn.py
@@ -6,14 +6,40 @@ from jax import random
 import jax.numpy as np
 
 
-def pca_orthogonal(v, dtype=np.float32):
-    def init(key, shape, dtype=dtype):
-        return v[: shape[0], : shape[1]]
+def constant_2d(m, dtype=np.float32):
+    """
+    Constant weight initializer
+    Args:
+        m: constant matrix
+        dtype: matrix dtype
 
+    Returns:
+        init function
+    """
+    def init(key, shape, dtype=dtype):
+        return m[:shape[0], :shape[1]]
+    return init
+
+
+def constant(matrix, dtype=np.float32):
+    def init(key, shape, dtype=dtype):
+        if matrix.shape != shape:
+            raise ValueError(f"input array of shape {matrix.shape} is not equal to weight array of shape {shape}")
+        return matrix
     return init
 
 
 def homotopy_activation(x, alpha=1.0, activation_func=None):
+    """
+
+    Args:
+        x:
+        alpha:
+        activation_func:
+
+    Returns:
+
+    """
     if activation_func:
         return alpha * x + (1 - alpha) * activation_func(x)
     else:
@@ -21,7 +47,16 @@ def homotopy_activation(x, alpha=1.0, activation_func=None):
 
 
 def HomotopyDense(out_dim, W_init=zeros, b_init=zeros):
-    """Layer constructor function for a dense (fully-connected) layer."""
+    """
+    Layer constructor function for a dense (fully-connected) layer.
+    Args:
+        out_dim:
+        W_init:
+        b_init:
+
+    Returns:
+
+    """
 
     def init_fun(rng, input_shape):
         output_shape = input_shape[:-1] + (out_dim,)
